8e7d9fe5abf532472637f4e65390d468118c988c
greg0rys <shenefeltgregory@gmail.com>
2024-06-18 18:52:04 -0700

    added documentation

--- src/ManagerClasses/CheckedBooksManager.java
+++ src/ManagerClasses/CheckedBooksManager.java
@@ -1,11 +1,26 @@
+/**
+ * The manager class that will interact with the private table manager.
+ * Calls all methods that perform CRUD operations to the SQLite DB
+ * @version 061824
+ * @author greg0rys
+ */
 package ManagerClasses;
 
 import Data.CheckedBooksTableManager;
+import LibraryObjects.Book;
 
 public class CheckedBooksManager
 {
 
-    private static final CheckedBooksTableManager BOOKS_TABLE_MANAGER = new CheckedBooksTableManager();
+    private static final CheckedBooksTableManager BOOKS_TABLE_MANAGER = new CheckedBooksTableManager(); // statically create the field so only one instance.
+
+
+    /**
+     * Default no args constructor.
+     */
+    public CheckedBooksManager()
+    {
+    }
 
 
 

9cc9aabedd5cb697edf9c2bade802971a5321f85
greg0rys <shenefeltgregory@gmail.com>
2024-06-28 21:12:16 -0700

    Update FrameMaker interface and implement SearchByTitleForm
    This commit expands the FrameMaker interface description and applies this interface in the SearchByTitleForm class. It also includes changes in the design form to accommodate new UI components. The enhancements aim to standardize JFrame initialization, improve GUI structure, and simplify user interactions in the application.

--- src/Views/FrameMaker.java
+++ src/Views/FrameMaker.java
@@ -5,13 +5,15 @@ import javax.swing.*;
 public interface FrameMaker
 {
     /**
-     * To be called as the last line in all JFrames init
+     * To be called as the last line in all JFrames init.
+     * This interface will provide default methods for the JFrame classes; The typical Java multi inheritance hack.
      * @param frame the frame that we want to manage.
      */
     default void defaultSetup(JFrame frame)
     {
         if(frame == null)
             return;
+
         frame.setVisible(true);
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
